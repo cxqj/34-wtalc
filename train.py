9380d7518b38a58ac70957ca2127cddd9c937855
cxqj <43702702+cxqj@users.noreply.github.com>
2020-02-10 20:32:42 +0800

    Update train.py

--- train.py
+++ train.py
@@ -20,10 +20,10 @@ def MILL(element_logits, seq_len, batch_size, labels, device):
 
     k = np.ceil(seq_len/8).astype('int32')
     labels = labels / torch.sum(labels, dim=1, keepdim=True)
-    instance_logits = torch.zeros(0).to(device)
+    instance_logits = torch.zeros(0).to(device) #[]
     for i in range(batch_size):
-        tmp, _ = torch.topk(element_logits[i][:seq_len[i]], k=int(k[i]), dim=0)
-        instance_logits = torch.cat([instance_logits, torch.mean(tmp, 0, keepdim=True)], dim=0)
+        tmp, _ = torch.topk(element_logits[i][:seq_len[i]], k=int(k[i]), dim=0)  #沿给定dim维度返回输入张量input中 k 个最大值 (94,20)
+        instance_logits = torch.cat([instance_logits, torch.mean(tmp, 0, keepdim=True)], dim=0)  #(1,20)
     milloss = -torch.mean(torch.sum(Variable(labels) * F.log_softmax(instance_logits, dim=1), dim=1), dim=0)
     return milloss
 
@@ -32,23 +32,23 @@ def CASL(x, element_logits, seq_len, n_similar, labels, device):
         element_logits should be torch tensor of dimension (n_similar, n_element, n_class) 
         seq_len should be numpy array of dimension (B,)
         labels should be a numpy array of dimension (B, n_class) of 1 or 0 '''
-
+   
     sim_loss = 0.
     n_tmp = 0.
     for i in range(0, n_similar*2, 2):
-        atn1 = F.softmax(element_logits[i][:seq_len[i]], dim=0)
-        atn2 = F.softmax(element_logits[i+1][:seq_len[i+1]], dim=0)
+        atn1 = F.softmax(element_logits[i][:seq_len[i]], dim=0)  #(750,20)
+        atn2 = F.softmax(element_logits[i+1][:seq_len[i+1]], dim=0)  #(54,20)
 
-        n1 = torch.FloatTensor([np.maximum(seq_len[i]-1, 1)]).to(device)
-        n2 = torch.FloatTensor([np.maximum(seq_len[i+1]-1, 1)]).to(device)
-        Hf1 = torch.mm(torch.transpose(x[i][:seq_len[i]], 1, 0), atn1)
-        Hf2 = torch.mm(torch.transpose(x[i+1][:seq_len[i+1]], 1, 0), atn2)
-        Lf1 = torch.mm(torch.transpose(x[i][:seq_len[i]], 1, 0), (1 - atn1)/n1)
-        Lf2 = torch.mm(torch.transpose(x[i+1][:seq_len[i+1]], 1, 0), (1 - atn2)/n2)
+        n1 = torch.FloatTensor([np.maximum(seq_len[i]-1, 1)]).to(device)  #749
+        n2 = torch.FloatTensor([np.maximum(seq_len[i+1]-1, 1)]).to(device)  #53
+        Hf1 = torch.mm(torch.transpose(x[i][:seq_len[i]], 1, 0), atn1)              #(2048,20)
+        Hf2 = torch.mm(torch.transpose(x[i+1][:seq_len[i+1]], 1, 0), atn2)          #(2048,20)
+        Lf1 = torch.mm(torch.transpose(x[i][:seq_len[i]], 1, 0), (1 - atn1)/n1)     #(2048,20)
+        Lf2 = torch.mm(torch.transpose(x[i+1][:seq_len[i+1]], 1, 0), (1 - atn2)/n2) #(2048,20)
 
-        d1 = 1 - torch.sum(Hf1*Hf2, dim=0) / (torch.norm(Hf1, 2, dim=0) * torch.norm(Hf2, 2, dim=0))
-        d2 = 1 - torch.sum(Hf1*Lf2, dim=0) / (torch.norm(Hf1, 2, dim=0) * torch.norm(Lf2, 2, dim=0))
-        d3 = 1 - torch.sum(Hf2*Lf1, dim=0) / (torch.norm(Hf2, 2, dim=0) * torch.norm(Lf1, 2, dim=0))
+        d1 = 1 - torch.sum(Hf1*Hf2, dim=0) / (torch.norm(Hf1, 2, dim=0) * torch.norm(Hf2, 2, dim=0)) #20
+        d2 = 1 - torch.sum(Hf1*Lf2, dim=0) / (torch.norm(Hf1, 2, dim=0) * torch.norm(Lf2, 2, dim=0)) #20
+        d3 = 1 - torch.sum(Hf2*Lf1, dim=0) / (torch.norm(Hf2, 2, dim=0) * torch.norm(Lf1, 2, dim=0)) #20
 
         sim_loss = sim_loss + 0.5*torch.sum(torch.max(d1-d2+0.5, torch.FloatTensor([0.]).to(device))*Variable(labels[i,:])*Variable(labels[i+1,:]))
         sim_loss = sim_loss + 0.5*torch.sum(torch.max(d1-d3+0.5, torch.FloatTensor([0.]).to(device))*Variable(labels[i,:])*Variable(labels[i+1,:]))
@@ -59,14 +59,14 @@ def CASL(x, element_logits, seq_len, n_similar, labels, device):
 
 def train(itr, dataset, args, model, optimizer, logger, device):
 
-    features, labels = dataset.load_data(n_similar=args.num_similar)
-    seq_len = np.sum(np.max(np.abs(features), axis=2) > 0, axis=1)
-    features = features[:,:np.max(seq_len),:]
+    features, labels = dataset.load_data(n_similar=args.num_similar) # (6,750,2048)/(6,20)
+    seq_len = np.sum(np.max(np.abs(features), axis=2) > 0, axis=1) #[264,101,750,463,473,750]  获取有效的特征长度
+    features = features[:,:np.max(seq_len),:] #(6,750,2048)
 
     features = torch.from_numpy(features).float().to(device)
     labels = torch.from_numpy(labels).float().to(device)
 
-    final_features, element_logits = model(Variable(features))
+    final_features, element_logits = model(Variable(features)) #(6,750,2048)/(6,750,20)
         
     milloss = MILL(element_logits, seq_len, args.batch_size, labels, device)
     casloss = CASL(final_features, element_logits, seq_len, args.num_similar, labels, device)
